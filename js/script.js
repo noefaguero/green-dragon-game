569d3e127d6bdd0618839a5ef407f7fe0c0f6c2e
Noé Fernández Agüero <noefernandezaguero@MacBook-Pro-de-Noe.local>
2023-10-29 12:11:20 +0100

    Functionality to create the dragons

--- js/script.js
+++ js/script.js
@@ -1,48 +1,65 @@
-const greendragon = document.getElementById("green-dragon")
+let green
 const visor = document.getElementById("visor")
 const container = document.getElementById("container")
 
 
 //////////////////////////// FUNCTIONS //////////////////////////////
+
+const createDragon = (color) => {
+    let dragon = document.createElement("IMG")
+    dragon.setAttribute("class", color)
+    dragon.setAttribute("src", `assets/images/${color}.png`)
+    visor.append(dragon)
+}
+
 const moveDragon = (event) => {
     if(event.target.tagName === "IMG"){
-        greendragon.style.top = (parseInt(greendragon.style.top) + (event.layerY - Math.round(event.target.clientHeight/2))) + "px"
+        green.style.top = (parseInt(green.style.top) + (event.layerY - Math.round(event.target.clientHeight/2))) + "px"
     } else {
-        greendragon.style.top = event.layerY + "px"
+        green.style.top = event.layerY + "px"
     }
 }
 
-const breatheFire = (direction) => {
+const breatheFire = (dragon) => {
+    // create fire element
     let fire = document.createElement("IMG")
     fire.setAttribute("class", "fire")
     fire.setAttribute("src", "assets/images/fire.gif")
-    fire.style.top = greendragon.style.top
-    fire.style.left = "950px"
+    // set direction
+    let direction = 1
+    if (dragon.classList.contains("green")) {
+        direction = -1
+    }
+    // initial position
+    if (direction == 1) {
+        fire.style.top = dragon.style.top
+        fire.style.left = dragon.style.left
+    } else if (direction == -1) {
+        fire.style.top = (parseInt(dragon.style.top) - 10) + "px"
+        let posX = window.getComputedStyle(dragon)
+        fire.style.left = (parseInt(posX.left) - 100) + "px"
+    }
     visor.append(fire)
-
+    // move fire 1px for each 1ms
     setInterval(() => moveFire(direction,fire), 1)
-
-    
 }
 
+const moveFire = (direction, object) => {
+    
+    position = parseInt(object.style.left)
 
-const moveFire = (direction,object) => {
-    position = parseInt(object.style.left);
-    if (direction === 1) {
-        if (position >= (visor.clientWidth - object.clientWidth/2)) {
-        } else {
-            object.remove()
-        }
-    } else if (direction === -1){
-        if (position > (0 - object.clientWidth/2)) {
-            object.style.left = (position + direction) + "px"
-        } else {
-            object.remove()
-        }
+    if ( position > (0 - object.clientWidth/2) && position < (visor.clientWidth - object.clientWidth/2)) {
+        object.style.left = (position + direction) + "px"
+    } else {
+        object.remove()
     }
 }
 
 
 ////////////////////////////// EVENTS ////////////////////////////////
+document.addEventListener("DOMContentLoaded", () => {
+    createDragon("green")
+    green = document.querySelector(".green")
+})
 visor.addEventListener("mousemove", (event) => moveDragon(event))
-visor.addEventListener("click", () => breatheFire(-1))
+visor.addEventListener("click", () => breatheFire(green))
