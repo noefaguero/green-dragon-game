ce75f15632150afd4d0798e6abfe15bcd4d7f0bd
Noé Fernández Agüero <noefernandezaguero@MacBook-Pro-de-Noe.local>
2023-10-29 08:55:00 +0100

    Functionality: the dragons breath fire

--- js/script.js
+++ js/script.js
@@ -3,19 +3,46 @@ const visor = document.getElementById("visor")
 const container = document.getElementById("container")
 
 
-
+//////////////////////////// FUNCTIONS //////////////////////////////
 const moveDragon = (event) => {
     if(event.target.tagName === "IMG"){
-        if (event.layerY > event.target.clientHeight/2) {
-            console.log( event.layerY)
-            greendragon.style.top = (parseInt(greendragon.style.top) + (event.layerY - Math.round(event.target.clientHeight/2))) + "px"
-        } else {
-            greendragon.style.top = (parseInt(greendragon.style.top) + (event.layerY - Math.round(event.target.clientHeight/2))) + "px"
-        }
+        greendragon.style.top = (parseInt(greendragon.style.top) + (event.layerY - Math.round(event.target.clientHeight/2))) + "px"
     } else {
         greendragon.style.top = event.layerY + "px"
     }
 }
 
+const breatheFire = (direction) => {
+    let fire = document.createElement("IMG")
+    fire.setAttribute("class", "fire")
+    fire.setAttribute("src", "assets/images/fire.gif")
+    fire.style.top = greendragon.style.top
+    fire.style.left = "950px"
+    visor.append(fire)
 
+    setInterval(() => moveFire(direction,fire), 1)
+
+    
+}
+
+
+const moveFire = (direction,object) => {
+    position = parseInt(object.style.left);
+    if (direction === 1) {
+        if (position >= (visor.clientWidth - object.clientWidth/2)) {
+        } else {
+            object.remove()
+        }
+    } else if (direction === -1){
+        if (position > (0 - object.clientWidth/2)) {
+            object.style.left = (position + direction) + "px"
+        } else {
+            object.remove()
+        }
+    }
+}
+
+
+////////////////////////////// EVENTS ////////////////////////////////
 visor.addEventListener("mousemove", (event) => moveDragon(event))
+visor.addEventListener("click", () => breatheFire(-1))
